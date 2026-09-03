1947f97514dfb651fe2227e3f76b1bb909b7bd0c
christof1997 <Christof.Ignacy16@gmail.com>
2026-09-03 03:22:27 +0200

    Simplify XML documentation for `AsRef` in `NullableExtensions`.

--- CSDL/Extensions/NullableExtensions.cs
+++ CSDL/Extensions/NullableExtensions.cs
@@ -12,11 +12,7 @@ namespace CSDL.Extensions {
         /// </summary>
         /// <remarks>
         ///     Use this to pass an optional struct to a P/Invoke import that takes it by <c>in</c> - SDL
-        ///     treats a null pointer there as "unset"/"use the default". <paramref name="storage"/> backs
-        ///     the returned reference and must outlive it, so declare it inline at the call site
-        ///     (<c>nullable.AsRef(out Rect storage)</c>). <see cref="UnscopedRefAttribute"/> is required
-        ///     here because the compiler otherwise scopes <c>out</c> parameters of an unconstrained struct
-        ///     type parameter to the method body, on the chance <typeparamref name="T"/> is a ref struct.
+        ///     treats a null pointer there as "unset"/"use the default".
         /// </remarks>
         internal static ref readonly T AsRef<T>(this T? nullable, [UnscopedRef] out T storage) where T : struct {
             storage = nullable.GetValueOrDefault();
